d967135e0b3a655ec7d119abe9ff9f72703265de
Douglas Ronne <vextorspace@gmail.com>
2024-05-29 14:45:22 -0400

    removed title and refactored to a card

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,10 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import model.Item
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -19,22 +16,7 @@ fun App() {
             Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            makeCard("Create a card", "- create card body")
-        }
-    }
-}
-
-private fun makeCard(title: String, content: String) {
-    Card(
-        modifier = Modifier.padding(6.dp),
-        elevation = 3.dp
-    ) {
-        Column {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.h5
-            )
-            Text(content)
+            Item("Create a card", "- create card body").makeCard()
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/model/Item.kt
@@ -0,0 +1,22 @@
+package model
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class Item(val title: String, val content: String) {
+    @Composable fun makeCard() {
+        Card(
+            modifier = Modifier.padding(6.dp),
+            elevation = 3.dp
+        ) {
+            Column {
+                Text(content)
+            }
+        }
+    }
+}

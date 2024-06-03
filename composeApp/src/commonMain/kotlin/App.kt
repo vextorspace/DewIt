e37f9ea6f3e29cfbd08fba0c9db995373168b231
Douglas Ronne <vextorspace@gmail.com>
2024-06-03 08:39:20 -0400

    removed unused imports

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,19 +1,7 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import model.Item
 import model.ItemList
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import view.ItemCardMaker
 
 
 @Composable

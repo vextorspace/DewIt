f028a8687a706ee70f8083c1233b0d1ef714675c
Douglas Ronne <vextorspace@gmail.com>
2024-06-04 06:18:45 -0400

    works well on android, not so good on desktop

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,15 +1,28 @@
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.*
 import model.Item
 import model.ItemList
 import view.ItemListCardMaker
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Preview
 fun App() {
-    val itemLists = listOf(ItemList(listOf(Item("Card 1"), Item("Card 2"), Item("Card 3")), "Inbox"))
+    val itemLists = listOf(
+        ItemList(listOf(Item("Card 01"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 11"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 21"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 31"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 41"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 51"), Item("Card 2"), Item("Card 3")), "Inbox"),
+        ItemList(listOf(Item("Card 61"), Item("Card 2"), Item("Card 3")), "Inbox"),
+    )
+    val pagerState = rememberPagerState{5}
 
-    HorizontalPager() {
+    HorizontalPager(state=pagerState) { page ->
         itemLists.forEach { ItemListCardMaker(it).compose() }
     }
 }

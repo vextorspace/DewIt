61f52f2e6ba04c9601252b088cd5a388f9171fc5
Douglas Ronne <vextorspace@gmail.com>
2024-06-05 08:57:59 -0400

    desktop vertical scrolling working

--- composeApp/src/desktopMain/kotlin/view/VerticalDisplay.desktop.kt
+++ composeApp/src/desktopMain/kotlin/view/VerticalDisplay.desktop.kt
@@ -1,19 +1,25 @@
 package view
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import model.Item
 
 actual class VerticalDisplay actual constructor() {
     @Composable
     actual fun Compose(itemList: SnapshotStateList<Item>) {
         val selectedCard = remember { mutableStateOf<Item?>(null)}
+        val scrollState = rememberLazyListState()
 
         val onDeleteItem: (Item) -> Unit = {
             val index = itemList.indexOf(it)
@@ -22,17 +28,24 @@ actual class VerticalDisplay actual constructor() {
                 itemList.removeAt(index)
             }
         }
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(itemList) { item ->
-                key(item) {
-                    ItemCardMaker(
-                        item,
-                        selectedCard,
-                        onDeleteItem
-                    ).compose()
+        Row {
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                state = scrollState,
+                modifier = Modifier.weight(1f)
+            ) {
+                items(itemList) { item ->
+                    key(item) {
+                        ItemCardMaker(
+                            item,
+                            selectedCard,
+                            onDeleteItem
+                        ).compose()
+                    }
                 }
             }
-        }    }
+
+            VerticalScrollbar(adapter = rememberScrollbarAdapter(scrollState))
+        }
+    }
 }

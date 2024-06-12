553d64acff5038831fa6fdf714abe98deee0bf73
Douglas Ronne <vextorspace@gmail.com>
2024-06-12 18:58:35 -0400

    can edit a single card when there are multiples with the same contents

--- composeApp/src/commonMain/kotlin/view/HorizontalDisplay.kt
+++ composeApp/src/commonMain/kotlin/view/HorizontalDisplay.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import model.ItemList
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class HorizontalDisplay() {
     @Composable
     fun Compose(itemLists: MutableState<MutableList<ItemList>>)

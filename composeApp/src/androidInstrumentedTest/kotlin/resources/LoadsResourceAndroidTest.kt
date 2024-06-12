a4f9ebfded9494751642c1b4e9389dfb62621c6d
Douglas Ronne <vextorspace@gmail.com>
2024-06-12 09:44:21 -0400

    put test in package

--- composeApp/src/androidInstrumentedTest/kotlin/resources/LoadsResourceAndroidTest.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/resources/LoadsResourceAndroidTest.kt
@@ -1,6 +1,7 @@
+package resources
+
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
-import resources.Resource
 import kotlin.test.Test
 
 class LoadsResourceAndroidTest {

75c8607a8ac25f10eca5ce1b55646db9d5cc57b1
Douglas Ronne <vextorspace@gmail.com>
2024-06-12 18:51:01 -0400

    made sure UUID always going to be unique even when starting program again (doesn't go back to same state)

--- composeApp/src/commonTest/kotlin/ids/UUIDTest.kt
+++ composeApp/src/commonTest/kotlin/ids/UUIDTest.kt
@@ -2,6 +2,7 @@ package ids
 
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldNotBe
+import resources.Resource
 import kotlin.test.Test
 
 class UUIDTest {
@@ -17,5 +18,15 @@ class UUIDTest {
         val uuid1 = UUID.generateUUID()
         val uuid2 = UUID.generateUUID()
         uuid1.shouldNotBe(uuid2)
+    }
+
+
+    @Test
+    fun `when first loaded should not return the same uuid`() {
+        val regressionTestResource = Resource("/resources/uuidRegressionTest.txt")
+        val uuid1 = UUID.generateUUID()
+        val uuid2 = regressionTestResource.readTextFromFile()
+        uuid1.shouldNotBe(uuid2)
+        regressionTestResource.writeTextToFile(uuid1)
     }
 }

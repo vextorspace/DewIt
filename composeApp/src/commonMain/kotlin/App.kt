7a7e8d0ef8c2eb1e2dfc85aae857d0971c9d0928
Douglas Ronne <vextorspace@gmail.com>
2024-06-21 11:24:24 -0400

    redid basic list for gtd

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -19,7 +19,7 @@ import view.actions.OnAddCard
 @Preview
 fun App() {
     val itemList = remember {
-        mutableStateOf(createFakeItemLists().toMutableList())
+        mutableStateOf(createGtdList().toMutableList())
     }
     val statusText = remember {  mutableStateOf("Status: ${itemList.value.size}")  }
 
@@ -57,67 +57,31 @@ private fun deleteItemFromList(
 }
 
 
-private fun createFakeItemLists(): List<Item> {
+private fun createGtdList(): List<Item> {
     val itemList = listOf(
 
                 Item(
                 "Inbox",
                 listOf(
-                    Item("Card 01"),
-                    Item("Card 2"),
-                    Item("Card 3"),
-                    Item("Card 4"),
-                    Item("Card 5"),
-                    Item("Card 6"),
-                    Item("Card 7"),
-                    Item("Card 8"),
-                    Item("Card 9"),
-                    Item("Card 10"),
-                    Item("Card 11"),
-                    Item("Card 12"),
-                    Item("Card 13"),
-                    Item("Card 14"),
-                    Item("Card 15"),
-                    Item("Card 16"),
-                    Item("Card 17"),
-                    Item("Card 18"),
-                    Item("Card 19"),
-                    Item("Card 20"),
-                    Item("Card 21"),
-                    Item("Card 22"),
-                    Item("Card 23"),
-                    Item("Card 24"),
-                    Item("Card 25"),
-                    Item("Card 26"),
-                    Item("Card 27"),
-                    Item("Card 28"),
-                    Item("Card 29")
+                    Item("Review How You Use This App")
                 )
         ),
         Item(
-            "Inbox",
-            listOf(Item("Card 11", mutableListOf(Item("Sub 1"), Item("Sub 2", mutableListOf(Item("Sub Sub"))))), Item("Card 2"), Item("Card 3"))
+            "Todo",
+            listOf(Item("Read GTD Book"), Item("Do a Dump"))
         ),
         Item(
-            "Inbox",
-            listOf(Item("Card 21"), Item("Card 2"), Item("Card 3"))
+            "Projects"
         ),
         Item(
-            "Inbox",
-            listOf(Item("Card 31"), Item("Card 2"), Item("Card 3"))
+            "Someday Maybe"
         ),
         Item(
-            "Inbox",
-            listOf(Item("Card 41"), Item("Card 2"), Item("Card 3"))
+            "Waiting On"
         ),
         Item(
-            "Inbox",
-            listOf(Item("Card 51"), Item("Card 2"), Item("Card 3"))
-        ),
-        Item(
-            "Inbox",
-            listOf(Item("Card 61"), Item("Card 2"), Item("Card 3"))
-        ),
+            "References"
+        )
     )
 
     return itemList

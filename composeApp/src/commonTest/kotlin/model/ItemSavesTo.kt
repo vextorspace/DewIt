11f8f5838bb31ddd5ce64da62816928bf92ed680
Douglas Ronne <vextorspace@gmail.com>
2024-07-01 07:02:57 -0400

    also serializes nested items

--- composeApp/src/commonTest/kotlin/model/ItemSavesTo.kt
+++ composeApp/src/commonTest/kotlin/model/ItemSavesTo.kt
@@ -24,4 +24,33 @@ class ItemSavesToJsonTest {
         """.trimIndent()
         itemJson shouldEqualJson expectedJson
     }
+
+    @Test
+    fun `nested items save to json`() {
+        // Given
+        val content = "::ITEM CONTENT::"
+        val id = "::SOME_UUID::"
+        val subItemContent = "::SUB ITEM CONTENT::"
+        val subItemId = "::SUB ITEM UUID::"
+        val subItem = Item(subItemContent, mutableListOf(), subItemId)
+        val item = Item(content, mutableListOf(subItem), id)
+
+        // When
+        val itemJson = item.toJson()
+
+        // Then
+        val expectedJson = """
+            {
+                "content": "$content",
+                "id": "$id",
+                "subItems": [
+                    {
+                        "content": "$subItemContent",
+                        "id": "$subItemId"
+                    }
+                ]
+            }
+        """.trimIndent()
+        itemJson shouldEqualJson expectedJson
+    }
 }

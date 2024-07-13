12a873ed67186c72e4d775166fd73ebe99411167
Douglas Ronne <vextorspace@gmail.com>
2024-07-13 11:44:47 -0400

    copy workflow puts item in destination without removing from original parent

--- composeApp/src/commonMain/kotlin/model/ItemWorkflow.kt
+++ composeApp/src/commonMain/kotlin/model/ItemWorkflow.kt
@@ -3,8 +3,12 @@ package model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ItemWorkflow(val destination: Item, val actionType: ActionType = ActionType.Copy) {
-
-
-
+data class ItemWorkflow(val item: Item, val destination: Item, val actionType: ActionType = ActionType.Copy) {
+    fun execute() {
+        when(actionType) {
+            ActionType.Copy -> {
+                destination.add(item)
+            }
+        }
+    }
 }
--- composeApp/src/commonTest/kotlin/model/ItemWorkflowTest.kt
+++ composeApp/src/commonTest/kotlin/model/ItemWorkflowTest.kt
@@ -1,5 +1,6 @@
 package model
 
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.test.Test
@@ -9,11 +10,12 @@ class ItemWorkflowTest {
     @Test
     fun `ItemWorkflow contains destination item`() {
         // Given
+        val item = Item()
         val destinationItem = Item()
-        val itemWorkflow = ItemWorkflow(destinationItem)
+        val itemWorkflow = ItemWorkflow(item, destinationItem)
 
         // When
-        val destination = itemWorkflow.destination
+        val destination = itemWorkflow.item
 
         // Then
         destination.shouldBeInstanceOf<Item>()
@@ -22,8 +24,9 @@ class ItemWorkflowTest {
     @Test
     fun `ItemWorkflow contains actionType defaults to Copy`() {
         // Given
+        val item = Item()
         val destinationItem = Item()
-        val itemWorkflow = ItemWorkflow(destinationItem)
+        val itemWorkflow = ItemWorkflow(item, destinationItem)
 
         // When
         val actionType = itemWorkflow.actionType
@@ -32,5 +35,23 @@ class ItemWorkflowTest {
         actionType.shouldBeInstanceOf<ActionType>()
 
         actionType.shouldBe(ActionType.Copy)
+    }
+
+    @Test
+    fun `When executing the copy action on an ItemWorkflow it should put a copy of the item in the destination`() {
+        // Given
+        val item = Item("parent")
+        val child = Item("child")
+        val copyToParent = Item("copy to parent")
+        item.add(child)
+
+        val itemWorkflow = ItemWorkflow(child, copyToParent, ActionType.Copy)
+
+        // When
+        itemWorkflow.execute()
+
+        // Then
+        copyToParent.subItems.shouldContainExactly(child)
+        item.subItems.shouldContainExactly(child)
     }
 }
